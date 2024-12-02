b422cf3e3771a2578db2a6c852143c1eecf4579d
RIshabh Rai <rishabh@euracle.com>
2024-12-02 20:13:13 +0530

    Added ads

---
+++ src/app/popunder.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PopunderDirective } from './popunder.directive';
+
+describe('PopunderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PopunderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
